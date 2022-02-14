7bd096bc6fbb98bff9d58391431fabf8433a7078
Maherymila <elyna44raharinirina@gmail.com>
2022-02-14 09:12:29 +0300

    ici les twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -19,9 +19,15 @@
       */
         public function show($slug)
         {
+            $answers = [
+                'Make sure your cat is sitting purrrfectly still ?',
+                'Honestly, I like furry shoes better than MY cat',
+                'Maybe... try saying the spell backwards?',
+            ];
 
             return $this->render('question/show.html.twig', [
-                'question' => ucwords(str_replace('-', ' ', $slug))
+                'question' => ucwords(str_replace('-', ' ', $slug)),
+                'answers' => $answers,
             ]);
             
 
